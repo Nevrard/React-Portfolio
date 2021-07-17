d3c045e8d6d8c45ca4c04bd243deeab81ad9a91f
Nevrard <ndangaevrard@gmail.com>
2021-07-17 13:48:52 -0400

    adding resume

---
+++ src/components/Resume.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Resume from "../assets/COLLIN_HODGSON_Resume.pdf";
+
+const AboutMe = () => {
+  return (
+    <MDBContainer className="mt-5">
+      <MDBRow>
+        <MDBCol md="12">
+          <embed id="Resume" src={Resume} width="100%" height="750px" />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default AboutMe;
