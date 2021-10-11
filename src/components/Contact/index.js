79ab68aa9f0ad37e0b7610b1e567a60faa18a99b
Nevrard <ndangaevrard@gmail.com>
2021-10-11 13:07:37 -0400

    adding skills to my portfolio

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -92,10 +92,27 @@ const ContactInfo = () => {
                       )}
             </div>
         </MDBCol>
-        <MDBCol md="4" className="text-center">
+        <MDBCol md="5" className="text-center">
         <h1 className="title">Skills</h1>
         <div className="skills">
-          <h2 className="Skillstitle">Languages  and tools</h2>
+          <h4 className="title"> <span role="img">💻</span>Languages  and tools:</h4>
+            <img alt="html" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"/>
+            <img alt="html" src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/MongoDB-00000F?style=for-the-badge&logo=MongoDB&logoColor=white"/>
+        
+          <h4 className="title"> <span role="img">🚀</span>Frameworks Known:</h4>   
+
+          <img alt="html" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node-dot-js&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white"/>
+            <img alt="html" src="https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white"/> 
+            <img alt="html" src="https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white"/> 
+        
+          <h4 className="title"> <span role="img">🚀</span>IDE's Known:</h4>  
+        
         </div>
         
         </MDBCol>
